6bc0abf88262dc03c186479cb91a511aab85a720
maheshchhagani <maheshchhagani121212@gmail.com>
2024-04-25 23:11:17 +0530

    master

--- resizable-layout/resizable-backend/index.js
+++ resizable-layout/resizable-backend/index.js
@@ -4,14 +4,71 @@ const cors = require('cors');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+// vHAPyeaL2oPdJ3XX
+// const mongoURI = 'mongodb+srv://surajhemnani04:vHAPyeaL2oPdJ3XX@data-neuron-assessment.tcryenj.mongodb.net/?retryWrites=true&w=majority&appName=Data-neuron-assessment';
+
+const mongoURI = 'mongodb+srv://surajhemnani04:vHAPyeaL2oPdJ3XX@data-neuron-assessment.tcryenj.mongodb.net/';
+
+// Connection options
+const options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+};
+
+// Establish the connection
+mongoose.connect(mongoURI, options)
+	.then(() => {
+		console.log('Connected to MongoDB');
+	})
+	.catch((error) => {
+		console.error('Error connecting to MongoDB:', error.message);
+
+		// Handle specific error conditions
+		if (error.name === 'MongoNetworkError') {
+			console.error('Network error occurred. Check your MongoDB server.');
+		} else if (error.name === 'MongooseServerSelectionError') {
+			console.error('Server selection error. Ensure'
+				+ ' MongoDB is running and accessible.');
+		} else {
+			// Handle other types of errors
+			console.error('An unexpected error occurred:', error);
+		}
+	});
+
+// Handling connection events
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+	console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+	console.log('Connected to MongoDB');
+});
+
+db.on('disconnected', () => {
+	console.log('Disconnected from MongoDB');
+});
+
+// Gracefully close the connection when the application exits
+process.on('SIGINT', () => {
+	mongoose.connection.close(() => {
+		console.log('Mongoose connection is disconnected'
+		+ ' due to application termination');
+		process.exit(0);
+	});
+});
+
 app.use(bodyParser.json());
 app.use(cors());
 
 let userData = [];
 
 app.post('/api/add', (req, res) => {
-  const { email, password, experience } = req.body;
-  const newUser = { email, password, experience };
+  const { name, password, experience } = req.body;
+  const newUser = { name, password, experience };
   userData.push(newUser);
   console.log("added")
   console.log(newUser)
@@ -19,10 +76,10 @@ app.post('/api/add', (req, res) => {
 });
 
 app.put('/api/update', (req, res) => {
-  const { email, password, experience } = req.body;
-  const index = userData.findIndex(user => user.email === email);
+  const { name, password, experience } = req.body;
+  const index = userData.findIndex(user => user.name === name);
   if (index !== -1) {
-    userData[index] = { email, password, experience };
+    userData[index] = { name, password, experience };
     res.json({ message: 'User data updated successfully', user: userData[index] });
     console.log(userData[index])
   } else {
